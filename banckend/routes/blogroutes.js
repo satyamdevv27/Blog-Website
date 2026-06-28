cbc1d8ebfac45ad6c3fd1417fd7c4b279f8ff952
Satyam Verma <satyam.verma.og27@gmail.com>
2026-06-28 23:24:27 +0530

    now one user can  have many blogs and te user can acces his blog only if he sends his bearer token with ret request

--- banckend/routes/blogroutes.js
+++ banckend/routes/blogroutes.js
@@ -1,13 +1,14 @@
 // routes/blogroutes.js
 import express from "express"; 
+import protect from "../middleware/authMiddleware.js";
 import { create_blog, update_blog, delete_blog, getallblog } from '../controller/blogcontroller.js';
 
 const router = express.Router();
 
 // All paths route cleanly off the main base path
-router.get('/', getallblog);
-router.post('/', create_blog);
-router.put('/:id', update_blog);
-router.delete('/:id', delete_blog);
+router.get('/', protect , getallblog);
+router.post('/',  protect ,create_blog);
+router.put('/:id',  protect ,update_blog);
+router.delete('/:id', protect , delete_blog);
 
 export default router;
